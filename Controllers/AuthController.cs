96478ac11ac57db19bba7505117b11fd0b8035f9
Rishav Shah ESET0123 <rishav.shah@esyasoft.com>
2025-11-04 10:52:11 +0530

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MDMS_Frontend.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+using System.Diagnostics;
+using MDMS_Frontend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MDMS_Frontend.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Meter()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        public IActionResult UploadMeter()
+        {
+            return View();
+        }
+        public IActionResult OrgUnit()
+        {
+            return View();
+        }
+        public IActionResult Consumer()
+        {
+            return View();
+        }
+        public IActionResult Tariff()
+        {
+            return View();
+        }
+        public IActionResult TariffSlab()
+        {
+            return View();
+        }
+        public IActionResult TodRule()
+        {
+            return View();
+        }
+        public IActionResult User()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
